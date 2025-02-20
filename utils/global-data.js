a28a6af67d0ef5c288e7431c234b2490758cb701
Bandar Deterjen <bandardeterjen@gmail.com>
2025-02-20 22:24:59 +0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Bandar Deterjen';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Sabun & Parfum Laundry';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Jakarta | Bogor | Bekasi | Depok | Tangerang | Purwokerto.';
 
   return {
     name,
